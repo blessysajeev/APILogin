ff2618b569f6f2b48e51945255cae2bd80bc5838
blessy <blessysajeev2023a@mca.ajce.in>
2023-07-12 20:14:06 +0530

    new coomit

---
+++ api/apiapp/urls.py
@@ -0,0 +1,7 @@
+from .views import RegisterAPI,LoginAPI
+from django.urls import path
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+]
